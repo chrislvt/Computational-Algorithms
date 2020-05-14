16becbde1bc2d03364e2747c0a6371888529aab6
chrislvt <43555155+chrislvt@users.noreply.github.com>
2020-05-15 00:12:37 +0300

    Start!

---
+++ lab_05/gauss_method.py
@@ -0,0 +1,37 @@
+def bubble_max_row(m, col):
+    max_element = m[col][col]
+    max_row = col
+    for i in range(col + 1, len(m)):
+        if abs(m[i][col]) > abs(max_element):
+            max_element = m[i][col]
+            max_row = i
+    if max_row != col:
+        m[col], m[max_row] = m[max_row], m[col]
+
+
+def gaussMethod(m):
+    n = len(m)
+    for k in range(n - 1):
+        bubble_max_row(m, k)
+        for i in range(k + 1, n):
+            div = m[i][k] / m[k][k]
+            m[i][-1] -= div * m[k][-1]
+            for j in range(k, n):
+                m[i][j] -= div * m[k][j]
+
+    if is_singular(m):
+        print('The system has infinite number of answers...')
+        return
+
+    x = [0 for i in range(n)]
+    for k in range(n - 1, -1, -1):
+        x[k] = (m[k][-1] - sum([m[k][j] * x[j] for j in range(k + 1, n)])) / m[k][k]
+
+    return x
+
+def is_singular(m):
+    for i in range(len(m)):
+        if not m[i][i]:
+            return True
+
+    return False
---
+++ lab_05/main.py
@@ -0,0 +1,106 @@
+from math import *
+from gauss_method import *
+
+
+# Подынтегральная формула
+def f(tau, phi, teta):
+    lR = (2 * cos(teta)) / (1 - (sin(teta) ** 2) * (cos(phi) ** 2))
+    return (1 - exp(-1 * tau * lR)) * cos(teta) * sin(teta)
+
+
+# Полином Лежандра (рекурсивный способ нахождения)
+def P(x, n):
+    p = [1, x]
+    for i in range(2, n + 1):
+        p.append(((2 * i - 1) * x * p[i - 1] - (i - 1) * p[i - 2]) / i)
+
+    return p[n]
+
+
+# Производная полинома Лежандра
+def dP(x, n,):
+    return (n / (1 - x ** 2)) * (P(x, n - 1) - x * P(x, n))
+
+
+# Поиск корней полинома Лежандра методом Ньютона.
+def NewtonMethod(a, b, i, n):
+    xn = cos(pi * ((4 * i - 1) / (4 * n + 2)))
+    xn1 = xn - P(xn, n) / dP(xn, n)
+    while abs(xn1 - xn) > 1e-10:
+        xn = xn1
+        xn1 = xn - P(xn, n) / dP(xn, n)
+
+    return xn1
+
+
+# Поиск узлов t
+def FindNodes(n):
+    nodes = []
+    for i in range(1, n + 1):
+        nodes.append(NewtonMethod(-1, 1, i, n))
+
+    return nodes
+
+
+# Поиск коэффициентов A
+def FindCoefficients(nodes):
+    matrix = []
+    for i in range(len(nodes)):
+        matrix.append([])
+        for j in range(len(nodes)):
+            matrix[i].append(nodes[j] ** i)
+        matrix[i].append((1 - (-1) ** (i + 1)) / (i + 1))
+
+    return gaussMethod(matrix)
+
+
+# Квадратурная формула Гаусса
+def GaussQuadrature(tau, phi, n):
+    t = FindNodes(n)
+    a = FindCoefficients(t)
+
+    c = 0
+    d = pi / 2
+
+    sum = 0
+    for i in range(n):
+        teta = ((c + d) / 2) + ((c - d) / 2) * t[i]
+        sum += a[i] * f(tau, phi, teta)
+
+    sum = sum * (c - d) / 2
+
+    return sum
+
+
+# Последовательное интегрирование.
+def Magic(N, M, tau):
+
+    a = 0
+    b = pi / 2
+
+    # Господь всемогущий... это же для вычисления phi!
+    step = abs(b - a) / N
+    phi = a
+
+    sum = 0
+    for i in range(N // 2 - 1):
+        phi = a + 2 * i * step
+        sum += GaussQuadrature(tau, phi, M)
+
+        phi = a + 2 * (i + 1) * step
+        sum += 4 * GaussQuadrature(tau, phi, M)
+
+        phi = a + 2 * (i + 2) * step
+        sum += GaussQuadrature(tau, phi, M)
+
+    sum = sum * (step / 3)
+
+
+# Это был долгий путь...
+def Result(N, M, tau):
+    I = Magic(N, M, tau)
+    I *= pi / 4
+
+# n, m = map(int, input("Введите кол-во участков разбиения по phi и по teta: ").split())
+# tau = float(input("Введите tau: "))
+#GaussQuadrature(1, 3)
