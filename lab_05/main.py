c1febbaa45106359b8ad00b7ac7410e3f186bf68
chrislvt <43555155+chrislvt@users.noreply.github.com>
2020-05-16 03:47:06 +0300

    added menu

--- lab_05/main.py
+++ lab_05/main.py
@@ -112,60 +112,84 @@ def make_plot(xdata, ydata):
     plt.grid(True)
     plt.show()
 
-# n, m = map(int, input("Введите кол-во участков разбиения по phi и по teta: ").split())
 
-#Обычный
-# n = m = 3
-# tau = 0.005
-# x, y = [], []
-# while tau < 10:
-#     result = Result(n, m, tau)
-#     x.append(tau)
-#     y.append(result)
-#     tau += 0.1
-#
-# for i in range(len(y)):
-#     print(y[i])
-#
-# make_plot(x, y)
-#
-
-# Вывод на экран от изменения n
-# n, m = 10, 10
-# tau = 0.005
-# x, y = [], []
-#
-# for i in range(3, 10, 2):
-#     x, y = [], []
-#     tau = 0.005
-#     while tau < 10:
-#         result = Result(n, i, tau)
-#         x.append(tau)
-#         y.append(result)
-#         tau += 0.1
-#
-#     l = "N = " + str(n) + ", M =" + str(i)
-#     plt.plot(x, y, label=l)
-#     plt.legend()
-#     plt.grid(True)
-#
-# plt.show()
+print("Главное меню:")
+print("1 - Самостоятельно задать n и m.")
+print("2 - Вывести изменение по Симпсону.")
+print("3 - Вывести изменение по Гауссу.")
+print("4 - Таблица для tau.")
+print("0 - Выход.")
 
 
-# Клевая таблица B)
-n, m = 10, 10
-tau = 1.0
+t = True
+while t:
+    choice = int(input("Выберите действие: "))
 
-y1, y2 = [], []
-for i in range(3, 15):
-    result1 = Result(i, 10, tau)
-    result2 = Result(10, i, tau)
-    y1.append(result1)
-    y2.append(result2)
+    if choice == 1:
+        n = int(input("Введите кол-во участков разбиения по Симпсону: "))
+        m = int(input("Введите кол-во участков разбиения по Гауссу: "))
 
-table = PrettyTable()
-table.add_column("Кол-во узлов", [i for i in range(3, 15)])
-table.add_column("Изменение по Симпсону", y1)
-table.add_column("Изменение по Гауссу", y2)
+        tau = 0.005
+        xdata, ydata = [], []
+        while tau < 10:
+            result = Result(n, m, tau)
+            xdata.append(tau)
+            ydata.append(result)
+            tau += 0.1
 
-print(table)
+        for i in range(len(ydata)):
+            print(ydata[i])
+
+        make_plot(xdata, ydata)
+
+    elif choice == 2:
+        for i in range(3, 10, 2):
+            x, y = [], []
+            tau = 0.005
+            while tau < 10:
+                result = Result(i, 10, tau)
+                x.append(tau)
+                y.append(result)
+                tau += 0.1
+
+            lb = "N = " + str(i) + ", M =" + str(10)
+            plt.plot(x, y, label=lb)
+            plt.legend()
+            plt.grid(True)
+
+        plt.show()
+
+    elif choice == 3:
+        for i in range(3, 10, 2):
+            x, y = [], []
+            tau = 0.005
+            while tau < 10:
+                result = Result(10, i, tau)
+                x.append(tau)
+                y.append(result)
+                tau += 0.1
+
+            lb = "N = " + str(10) + ", M =" + str(i)
+            plt.plot(x, y, label=lb)
+            plt.legend()
+            plt.grid(True)
+
+        plt.show()
+
+    elif choice == 4:
+        y1, y2 = [], []
+        for i in range(3, 15):
+            result1 = Result(i, 10, 1)
+            result2 = Result(10, i, 1)
+            y1.append(result1)
+            y2.append(result2)
+
+        table = PrettyTable()
+        table.add_column("Кол-во узлов", [i for i in range(3, 15)])
+        table.add_column("Изменение по Симпсону", y1)
+        table.add_column("Изменение по Гауссу", y2)
+
+        print(table)
+
+    elif choice == 0:
+        t = False
