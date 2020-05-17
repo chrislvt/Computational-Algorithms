89c7349bf34125c6818d79894b0214081f4f48d5
chrislvt <43555155+chrislvt@users.noreply.github.com>
2020-05-17 03:27:05 +0300

    method 1 in func

--- lab_06/main.py
+++ lab_06/main.py
@@ -9,6 +9,16 @@ def left(y_cur, y_prev, h):
     return (y_cur - y_prev) / h
 
 
+# Метод односторонних производных
+def method_1(input_data, step):
+    output_data = []
+    for i in range(len(input_data) - 1):
+        output_data.append(right(input_data[i], input_data[i + 1], h))
+
+    output_data.append(left(data[len(input_data) - 1], data[len(input_data) - 2], h))
+
+    return output_data
+
 x = [1, 2, 3, 4, 5, 6]
 data = [0.571, 0.889, 1.091, 1.231, 1.333, 1.412]
 
@@ -16,12 +26,6 @@ table = [x, data, [], [], [], [], []]
 
 h = 1
 
-d1 = []
-
-# Вычисление производных методом односторонних производных.
-for i in range(len(data) - 1):
-    d1.append(right(data[i], data[i + 1], h))
-
-d1.append(left(data[len(data) - 1], data[len(data) - 2], h))
+d1 = method_1(data, h)
 
 print(d1)
