be205d960c8f467c2245ab1d60f91650b8d98a40
chrislvt <43555155+chrislvt@users.noreply.github.com>
2020-05-20 18:43:19 +0300

    method_2 fixed

--- lab_06/main.py
+++ lab_06/main.py
@@ -37,13 +37,13 @@ def method_1(input_data, step):
 
 # Центральная разносторонняя производная
 def method_2(input_data, step):
-    output_data = []
+    output_data = [center_x0(input_data[0], input_data[1], input_data[2], step)]
+
     length = len(input_data)
     for i in range(1, length - 1):
         output_data.append(center(input_data[i + 1], input_data[i - 1], step))
 
-    output_data.append(center_x0(input_data[0], input_data[1], input_data[2], step))
-    output_data.append(center_x0(input_data[length - 1], input_data[length - 2], input_data[length - 3], step))
+    output_data.append(center_xn(input_data[length - 1], input_data[length - 2], input_data[length - 3], step))
 
     return output_data
 
