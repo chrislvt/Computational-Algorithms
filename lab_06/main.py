1227ac94a6271e29283f7d408cd504d3c2d7892b
chrislvt <43555155+chrislvt@users.noreply.github.com>
2020-05-21 20:48:39 +0300

    reshape added

--- lab_06/main.py
+++ lab_06/main.py
@@ -3,26 +3,42 @@ from central_derivative import *
 from runge import *
 from reshape import *
 from second_central import *
+from prettytable import PrettyTable
 
 
 x = [1, 2, 3, 4, 5, 6]
-data = [0.571, 0.889, 1.091, 1.231, 1.333, 1.412]
+y = [0.571, 0.889, 1.091, 1.231, 1.333, 1.412]
 
-table = [x, data, [], [], [], [], []]
+table = [x, y, [], [], [], [], []]
 
 h = 1
 
-d1 = method_1(data, h)
-d2 = method_2(data, h)
-d3_1 = method_3_1(data, h)
-d3_2 = method_3_2(data, h)
-
-d5 = method_5(data, h)
+d1 = method_1(y, h)
+d2 = method_2(y, h)
+d3_1 = method_3_1(y, h)
+d3_2 = method_3_2(y, h)
+d4 = method_4(x, y)
+d5 = method_5(y, h)
 
 
 print(d1)
 print(d2)
 print(d3_1)
 print(d3_2)
-
+print(d4)
 print(d5)
+
+# table = PrettyTable()
+# table.add_column("x", x)
+# table.add_column("y", y)
+# table.add_column("1", ['{:.3f}'.format(num) for num in d1])
+# #table.add_column("2", ['{:.3f}'.format(num) for num in d2])
+# #table.add_column("3_1", ['{:.3f}'.format(num) for num in d3_1])
+# table.add_column("3_2", ['{:.3f}'.format(num) for num in d3_2])
+# table.add_column("5", ['{:.3f}'.format(num) for num in d5])
+
+
+
+
+
+# print(table)
--- lab_06/reshape.py
+++ lab_06/reshape.py
@@ -1,1 +1,21 @@
-# ниче не работает :(
+# ща как заработает
+
+def eta(y):
+    return 1 / y
+
+
+def ksi(x):
+    return 1 / x
+
+
+def reshape(y_cur, y_next, x_cur, x_next):
+    return (eta(y_next) - eta(y_cur)) / (ksi(x_next) - ksi(x_cur)) * (y_cur / x_cur) ** 2
+
+
+# Производная, полученная с помощью метода выравниающих переменных
+def method_4(xdata, ydata):
+    output_data = []
+    for i in range(len(ydata) - 1):
+        output_data.append(reshape(ydata[i], ydata[i + 1], xdata[i], xdata[i + 1]))
+
+    return output_data
