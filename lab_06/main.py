8ffbabb7b648dd7cd163a62f1304446bf9ddab57
chrislvt <43555155+chrislvt@users.noreply.github.com>
2020-05-22 00:58:44 +0300

    more cool

--- lab_06/main.py
+++ lab_06/main.py
@@ -18,6 +18,9 @@ data_12 = LeftOneSided(ydata, step)
 data_2 = Central(ydata, step)
 data_31 = RightRunge(ydata, step)
 data_32 = LeftRunge(ydata, step)
+data_41 = RightReshape(xdata, ydata)
+data_42 = LeftReshape(xdata, ydata)
+
 
 
 table = PrettyTable()
@@ -26,8 +29,11 @@ table.add_column("y", ydata)
 table.add_column("Правосторонная", data_11)
 table.add_column("Левосторонняя", data_12)
 table.add_column("Центральная", data_2)
-table.add_column("Рунже на основе правосторонней", data_31)
-table.add_column("Рунже на основе левосторонней", data_32)
+table.add_column("Рунже правая", data_31)
+table.add_column("Рунже левая", data_32)
+table.add_column("Выравнивающая правая", data_41)
+table.add_column("Выравнивающая левая", data_42)
+
 # table.add_column("5", ['{:.3f}'.format(num) for num in d5])
 
 
--- lab_06/reshape.py
+++ lab_06/reshape.py
@@ -1,5 +1,3 @@
-# ща как заработает
-
 def eta(y):
     return 1 / y
 
@@ -8,14 +6,27 @@ def ksi(x):
     return 1 / x
 
 
-def reshape(y_cur, y_next, x_cur, x_next):
+def rightreshape(y_cur, y_next, x_cur, x_next):
     return (eta(y_next) - eta(y_cur)) / (ksi(x_next) - ksi(x_cur)) * (y_cur / x_cur) ** 2
 
 
-# Производная, полученная с помощью метода выравниающих переменных
-def method_4(xdata, ydata):
-    output_data = []
-    for i in range(len(ydata) - 1):
-        output_data.append(reshape(ydata[i], ydata[i + 1], xdata[i], xdata[i + 1]))
+def leftreshape(y_cur, y_prev, x_cur, x_prev):
+    return (eta(y_cur) - eta(y_prev)) / (ksi(x_cur) - ksi(x_prev)) * (y_cur / x_cur) ** 2
 
-    return output_data
+
+def RightReshape(xdata, ydata):
+    result = []
+    for i in range(len(ydata) - 1):
+        result.append("{:.3f}".format(rightreshape(ydata[i], ydata[i + 1], xdata[i], xdata[i + 1])))
+
+    result.append("-")
+
+    return result
+
+
+def LeftReshape(xdata, ydata):
+    result = ["-"]
+    for i in range(1, len(ydata)):
+        result.append("{:.3f}".format(leftreshape(ydata[i], ydata[i - 1], xdata[i], xdata[i - 1])))
+
+    return result
